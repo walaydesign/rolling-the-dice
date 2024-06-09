df9603b52d3248f0164bcf1959b8754e31e7c97c
youzhen <yl301114@gmail.com>
2024-06-09 13:42:22 +0100

    修改

--- public/js/common.js
+++ public/js/common.js
@@ -12,11 +12,30 @@ $(".btn-popup").click(function() {
     $(popup).fadeIn(300);
 })
 
-$(".popup_bg, .popup_close").click(function() {
+$(".popup_bg").click(function() {
     $(".popup").fadeOut(300);
 })
+
+$(".popup_close").click(function() {
+    $(".popup").fadeOut(300);
+})
+
+$(".popup-full_close").click(function() {
+    $(".popup").fadeOut(300);
+})
+
+$(".popup").click(function (event) {
+    var popupContent = $(".popup_window");
+    if (!popupContent.is(event.target) && popupContent.has(event.target).length === 0) {
+        $(".popup").fadeOut(300);
+    }
+});
 
 var music = document.getElementById("bgMusic");
 window.onload = function() {
     music.play();
+}
+
+window.onload = function(){
+    $(".load-bg").fadeOut();
 }
